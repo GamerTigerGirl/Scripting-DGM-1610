5b8e10efe649493b5bf3c15851e15903421f2a88
Emily <10905022@uvu.edu>
2021-09-11 14:04:09 -0600

    Edited Codes

--- Prototype 1 - Tank Game/Assets/Scripts/CameraFollow.cs
+++ Prototype 1 - Tank Game/Assets/Scripts/CameraFollow.cs
@@ -7,13 +7,11 @@ public class CameraFollow : MonoBehaviour
     public GameObject tank;
     private Vector3 offset = new Vector3(0,50,-100);
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.position = tank.transform.position + offset;
--- Prototype 1 - Tank Game/Assets/Scripts/PlayerController.cs
+++ Prototype 1 - Tank Game/Assets/Scripts/PlayerController.cs
@@ -11,13 +11,11 @@ public class PlayerController : MonoBehaviour
     public float vInput;
 
 
-    // Start is called before the first frame update
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
         hInput = Input.GetAxis("Horizontal");
