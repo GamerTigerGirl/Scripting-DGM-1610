110b6040c14afc6fdaa4aeb71cccc0c020c0b1df
Emily <10905022@uvu.edu>
2021-10-04 03:13:34 -0600

    Added Projectile Movement

---
+++ Prototype 2 - Top Down/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryOutOfBounds : MonoBehaviour
+{
+    public float  topBound = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y > topBound)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
--- Prototype 2 - Top Down/Assets/Scripts/PlayerControl.cs
+++ Prototype 2 - Top Down/Assets/Scripts/PlayerControl.cs
@@ -9,13 +9,17 @@ public class PlayerControl : MonoBehaviour
     public float vInput;
 
     public float xRange = -14.09f;
-    public float yRange = 4.95f;
+    public float yRange = 5.00f;
 
-    // public float health;
+    public GameObject projectile;
+    public Vector3 offest = new Vector3(0,1,0);
+
+    //public float health;
 
     // Update is called once per frame
     void Update()
     {
+
         hInput = Input.GetAxis("Horizontal");
         vInput = Input.GetAxis("Vertical");
 
@@ -30,6 +34,24 @@ public class PlayerControl : MonoBehaviour
         if(transform.position.x > xRange)
         {
             transform.position = new Vector3( xRange, transform.position.y, transform.position.z);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectile, transform.position + offest, projectile.transform.rotation);
+        }
+
+        transform.Translate(Vector3.up * speed * vInput * Time.deltaTime);
+        transform.Translate(Vector3.right * speed * hInput * Time.deltaTime);
+        // Created a wall on the -y side
+        if(transform.position.y < -yRange)
+        {
+            transform.position = new Vector3(-yRange, transform.position.x, transform.position.z);
+        }
+        // Created a wall on the y side
+        if(transform.position.y > yRange)
+        {
+            transform.position = new Vector3( yRange, transform.position.x, transform.position.z);
         }
     }
 }
