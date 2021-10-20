97351b3a11692b05fa03352ff8b76f368337f1e5
Emily <10905022@uvu.edu>
2021-10-20 17:20:49 -0600

    Added Enemy Scripts and Enemy Base Mechanics

--- Prototype 3 - First Person Game/Assets/Scripts/Enemy.cs
+++ Prototype 3 - First Person Game/Assets/Scripts/Enemy.cs
@@ -1,18 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using System.Linq;
 
 public class Enemy : MonoBehaviour
 {
+    // Enemy Stats
+    public int curHP, maxHP, scoreToGive;
+    //Movement
+    public float moveSpeed, attackRange, yPathOffset;
+    // Coordinates for a path
+    private List<Vector3> path;
+    // Enemy Weaopn
+    private Weapon weapon;
+    // Target to follow
+    private GameObject target;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Get the Componets
+        weapon = GetComponent<Weapon>();
+        target = FindObjectOfType<PlayerController>().gameObject;
+        InvokeRepeating("UpdatePath", 0.0f, 0.5f);
+
+        curHP = maxHP;
     }
 
-    // Update is called once per frame
+    void UpdatePath()
+    {
+        //Calculate a path to the target
+        NavMeshPath navMeshPath = new NavMeshPath();
+        NavMesh.CalculatePath(transform.position, target.transform.position, NavMesh.AllAreas, navMeshPath);
+
+        path = navMeshPath.corners.ToList();
+    }
+    void ChaseTarget()
+    {
+        if(path.Count == 0)
+            return;
+
+        //Move towards the closet path
+        transform.position = Vector3.MoveTowards(transform.position, path[0] + new Vector3(0,yPathOffset,0), moveSpeed * Time.deltaTime);
+
+        if(transform.position == path[0] + new Vector3(0, yPathOffset, 0))
+            path.RemoveAt(0);
+    }
+
     void Update()
     {
-        
+        //Look at the target
+        Vector3 dir = (target.transform.position - transform.position).normalized;
+        float angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+        transform.eulerAngles = Vector3.up * angle;
+
+        // Calulate the distance between the enemy and the player
+        float dist = Vector3.Distance(transform.position, target.transform.position);
+        // If within attackerange shoot at player
+        if(dist <= attackRange)
+        {
+            if(weapon.CanShoot())
+                weapon.Shoot();
+        }
+        //If enemy is too far away chase after player
+        else
+        {
+            ChaseTarget();
+        }
+    
     }
+
 }
