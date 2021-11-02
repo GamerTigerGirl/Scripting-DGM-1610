beeac0944a0baf7a8e10225cc973fd9c49bc82d2
Emily <10905022@uvu.edu>
2021-11-02 00:27:03 -0600

    Added Health and Ammo Pickups and Rotation to Pickups

---
+++ Prototype 3 - First Person Game/Assets/Scripts/PickUp.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    public PickUpType type;
+    public int value;
+    
+    [Header("Bobbing Motion")]
+    public float rotationSpeed;
+    public float bobSpeed;
+    public float bobHeight;
+    private bool bobbingUp;
+
+    private Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;   
+    }
+
+    public enum PickUpType
+    {
+        Health,
+        Ammo
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            PlayerController player = other.GetComponent<PlayerController>();
+
+          switch(type)
+          {
+              case PickUpType.Health:
+              player.GiveHealth(value);
+              break;
+              case PickUpType.Ammo:
+              player.GiveAmmo(value);
+              break;
+              default:
+              print("Type not accepted");
+              break;
+          }
+
+          Destroy(gameObject);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //Rotates the PickUp around the Y-aixs
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+        //Bobbing the PickUp
+        Vector3 offset = (bobbingUp == true ? new Vector3(0, bobHeight / 2, 0) : new Vector3(0, -bobHeight / 2, 0));
+
+        transform.position = Vector3.MoveTowards(transform.position, startPos + offset, bobSpeed * Time.deltaTime);
+
+        if(transform.position == startPos + offset)
+            bobbingUp = !bobbingUp;
+    }
+}
--- Prototype 3 - First Person Game/Assets/Scripts/PlayerController.cs
+++ Prototype 3 - First Person Game/Assets/Scripts/PlayerController.cs
@@ -16,11 +16,11 @@ public class PlayerController : MonoBehaviour
     public int maxHP;
 
     [Header("Mouse Look")]
-    //Mouse look sensititvity
+    //Mouse looke sensititivity
     public float lookSensitivity;
     //Lowest down we can look
     public float maxLookX;
-    //Hightest up we can look
+    //Highest up we can look
     public float minLookX;
     //Current x rotation of the camera
     private float rotX;
@@ -40,7 +40,6 @@ public class PlayerController : MonoBehaviour
         camera = Camera.main;
         rb = GetComponent<Rigidbody>();
     }
-
     //Applies Damage to the player
     public void TakeDamage(int damage)
     {
@@ -49,42 +48,25 @@ public class PlayerController : MonoBehaviour
         if(curHP <= 0)
             Die();
     }
-    //If player health is zero or below the run Die
+    //If player health is reduced zero or below then run Die()
     void Die()
     {
 
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Move();
-        CamLook();
-        
-        //Fire Button
-        if(Input.GetButton("Fire1"))
-        {
-            if(weapon.CanShoot())
-                weapon.Shoot();
-        }
-        //Jump Button
-        if(Input.GetButtonDown("Jump"))
-            Jump();
-    }
-
+    
     void Move()
     {
         float x = Input.GetAxis("Horizontal") * moveSpeed;
         float z = Input.GetAxis("Vertical") * moveSpeed;
 
         //rb.velocity = new Vector3(x, rb.velocity.y, z); - old code
-        //Move direction relative to the camera
+            //Move direction relative to the camera
         Vector3 dir = transform.right * x + transform.forward * z;
         dir.y = rb.velocity.y;
         //Jump with direction relative to the camera
         rb.velocity = dir;
     }
-
+    
     void CamLook()
     {
         //Mouse look sensitivity
@@ -106,4 +88,32 @@ public class PlayerController : MonoBehaviour
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
     }
+
+    public void GiveHealth(int amountToGive)
+    {
+        curHP = Mathf.Clamp(curHP + amountToGive, 0, maxHP);
+    }
+
+    public void GiveAmmo(int amountToGive)
+    {
+        weapon.curAmmo = Mathf.Clamp(weapon.curAmmo + amountToGive, 0, weapon.maxAmmo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CamLook();
+        
+        //Fire Button
+        if(Input.GetButton("Fire1"))
+        {
+            if(weapon.CanShoot())
+                weapon.Shoot();
+        }
+        //Jump Button
+        if(Input.GetButtonDown("Jump"))
+            Jump();
+    }
+
 }
